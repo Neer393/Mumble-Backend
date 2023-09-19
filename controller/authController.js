c796078bc6b7c720b9fd66f81ee117202233eca3
Neer393 <108523406+Neer393@users.noreply.github.com>
2023-09-19 18:38:15 +0530

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -15,8 +15,7 @@ const sendToken = (user,statuscode,res)=>{
         expires:new Date(Date.now()+30*24*60*60*1000),
         domain:'https://mumble-neer393.netlify.app',
         secure: true,
-        sameSite:'none',
-        httpOnly:true
+        sameSite:'none'
     });
     user.password = undefined;
     res.status(statuscode).json({
